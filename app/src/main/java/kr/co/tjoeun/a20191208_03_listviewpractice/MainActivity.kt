6c5218a9b1abba02d34d8e5b9bbef18f21fc6003
cho881020 <kj_cho@idealidea.co.kr>
2019-12-14 10:40:04 +0900

    공부용 주석 추가22

--- app/src/main/java/kr/co/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     var userAdapter:UserAdapter? = null
 
+//    액티비티의 시작점
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
